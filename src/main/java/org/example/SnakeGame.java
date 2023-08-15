f6ef83122121ba6abf30afa034f60c98cbd61d2a
FaridaFatali <faridafatali.91@gmail.com>
2023-08-15 10:35:56 +0400

    Initial commit

---
+++ src/main/java/org/example/SnakeGame.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Practice: <a href="https://www.youtube.com/watch?v=bI6e6qjJ8JQ">...</a>
+ * @author Farida Fatali
+ * Creating Executable 2D Snake Game
+ */
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
